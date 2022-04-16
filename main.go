6a60e901346f1966f9ad6c491e2e4914e066a583
yungbender <tomko251@gmail.com>
2022-04-16 12:27:12 +0200

    add waiting on db on start

--- main.go
+++ main.go
@@ -3,12 +3,49 @@ package main
 import (
 	"bda/api"
 	crawlerCollector "bda/crawler_collector"
+	"bda/logger"
+	"bda/models"
 	pingerCollector "bda/pinger_collector"
 	"fmt"
 	"os"
+	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
+func WaitForDb(log logger.Logger) {
+	log.Info(logrus.Fields{}, "Waiting for DB")
+	for {
+		isLive := true
+		db, err := models.GetDb()
+		if err != nil {
+			isLive = false
+		}
+		rawDb, err := db.DB()
+		if err != nil {
+			isLive = false
+		}
+		err = rawDb.Ping()
+		if err != nil {
+			isLive = false
+		}
+		if isLive {
+			break
+		} else {
+			log.Info(logrus.Fields{}, "DB unavaiable, waiting")
+			time.Sleep(5 * time.Second)
+			continue
+		}
+	}
+	log.Info(logrus.Fields{}, "DB active, continuing")
+}
+
 func main() {
+	log := logger.Logger{
+		Prefix: "MAIN",
+	}
+	WaitForDb(log)
+
 	if len(os.Args) > 1 {
 		switch os.Args[1] {
 		case "pinger":
